a255085b72b7991b4301c6d78529d00ac279b39f
Callie Porter-Borden <porterhyphenborden@gmail.com>
2019-09-29 00:53:05 -0400

    updated landing page

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -8,6 +8,7 @@ export default class LandingPage extends Component {
             <div className='welcome'>
                 <h2>Welcome to the world of Erosion!</h2>
                 <p>Erosion is a single-player puzzle game where you, an alien colonist, are tasked with terraforming the terrain of a newly-discovered planet. Manipulate the grid of land tiles to guide the river to it's target location, earning points along the way for eroding tiles and leaving fertile soil along the river. Learn how to play the game and earn your place on the Leaderboard!</p>
+                <p className='credit'>Thanks to Chris Hofmeister for letting me code his game concept!</p>
             </div>
         )
     }
